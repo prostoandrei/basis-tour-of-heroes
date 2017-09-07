9251357172028d8b459516029c5355db543d94b6
prostoandrei <prostoandrei91@gmail.com>
2017-09-07 14:19:27 +0300

    finished part4

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
 var Node = require('basis.ui').Node;
 var Hero = require('./app/components/hero/index');
+var DataObject = require('basis.data').Object;
+var Dataset = require('basis.data').Dataset;
+
+var dataset = new Dataset({
+    items: [
+        { id: 1, title: 'Headcrab' },
+        { id: 2, title: 'Magnetto' },
+        { id: 3, title: 'Cyclop' },
+        { id: 4, title: 'Batman' },
+        { id: 5, title: 'Superman' },
+        { id: 6, title: 'Storm' },
+        { id: 7, title: 'Flash' },
+        { id: 8, title: 'Wolverine' }
+    ].map(function (value) {
+        return new DataObject({
+            data: {
+                id: value.id,
+                title: value.title
+            }
+        });
+    })
+});
 
 module.exports = require('basis.app').create({
     title: 'Basis tour of heroes',
@@ -7,16 +29,13 @@ module.exports = require('basis.app').create({
         return new Node({
             template: resource('./app/template/layout.tmpl'),
             childClass: Hero,
-            childNodes: [
-                { id: 1, title: 'Headcrab' },
-                { id: 2, title: 'Magnetto' },
-                { id: 3, title: 'Cyclop' },
-                { id: 4, title: 'Batman' },
-                { id: 5, title: 'Superman' },
-                { id: 6, title: 'Storm' },
-                { id: 7, title: 'Flash' },
-                { id: 8, title: 'Wolverine' }
-            ]
+            dataSource: dataset
         });
     }
 });
+
+// setTimeout(function() {
+//     dataset.remove(dataset.pick());
+// }, 1000)
+
+
--- src/app/components/hero/index.js
+++ src/app/components/hero/index.js
@@ -3,7 +3,7 @@ var Node = require('basis.ui').Node;
 module.exports = Node.subclass({
     template: resource('./templates/hero.tmpl'),
     binding: {
-        id: 'id',
-        title: 'title',
+        id: 'data:',
+        title: 'data:',
     }
 });
