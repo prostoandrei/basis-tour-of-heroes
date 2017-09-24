8a9fd30dfc49fe7f7c12e4eee354ce62aa0577eb
prostoandrei <prostoandrei91@gmail.com>
2017-09-24 14:14:55 +0300

    restyled

--- src/app/pages/dashboard/index.js
+++ src/app/pages/dashboard/index.js
@@ -6,11 +6,11 @@ module.exports = new Node({
     template: resource('./templates/dashboard.tmpl'),
     childClass: {
         template: `
-            <a class="col-1-4">
-                <div class="module hero">
-                    <h4>{title}</h4>
-                </div>
-            </a>
+        <a class="col">
+            <div class="module hero">
+                <h4>{title}</h4>
+            </div>
+        </a>
         `,
         binding: {
             id: 'data:',
